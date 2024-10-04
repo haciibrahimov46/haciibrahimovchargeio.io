d0d36b799524ae61bd2147afb51d696dcfc1516b
haciibrahimov46 <137991071+haciibrahimov46@users.noreply.github.com>
2024-10-04 19:07:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    const menuList = document.getElementById("menu-list");
+    menuList.classList.toggle("active"); // "active" sınıfını ekle/kaldır
+});
